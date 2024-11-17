0100e74c4f914e8ce9bd504033f693a5413c76f0
JungJungIn <call518@gmail.com>
2024-11-17 13:25:25 +0900

    Bugfix for OPENAI_BASE_URL

--- perplexis.py
+++ perplexis.py
@@ -32,7 +32,7 @@ import shutil
 
 ### Mandatory Keys 설정
 os.environ["OLLAMA_BASE_URL"] = st.secrets["KEYS"].get("OLLAMA_BASE_URL", "http://localhost:11434")
-os.environ["OPENAI_BASE_URL"] = st.secrets["KEYS"].get("OLLAMA_BASE_URL", "https://api.openai.com/v1")
+os.environ["OPENAI_BASE_URL"] = st.secrets["KEYS"].get("OPENAI_BASE_URL", "https://api.openai.com/v1")
 os.environ["OPENAI_API_KEY"] = st.secrets["KEYS"].get("OPENAI_API_KEY", "")
 os.environ["PINECONE_API_KEY"] = st.secrets["KEYS"].get("PINECONE_API_KEY", "")
 
